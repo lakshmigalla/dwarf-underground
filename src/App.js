8242ee6bf2ddaac4759e8b7323c70e1e3d44dc55
Lakshmi Galla <lgalla@purdue.edu>
2018-06-11 16:04:38 -0400

    Created a component for title and author

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header'
 import Article from './Article'
+import CommentShare from './CommentShare'
+import TitleAuthor from './TitleAuthor'
 
 class App extends Component {
   render() {
@@ -12,29 +14,13 @@ class App extends Component {
         
         <main className="expanded row">
           <div className="large-8 medium-12 columns article">
-            <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
             
-            
-            <div className="avatar">
-              <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-              <div className="author-info">
-                <p className="author-name">By Domri, son of Flug</p>
-                <p className="date">on 28 April, 3018 of the Third Age</p>
-              </div>
-            </div>
+            <TitleAuthor />
 
             <Article />
 
-            <div className="article-links">
-              <a className="article-link" href="#">
-                <i className="fa fa-comments-o"></i>
-                <span className="article-link-text">Comments</span>
-              </a>
-              <a className="article-link" href="#">
-                <i className="fa fa-share"></i>
-                <span className="article-link-text">Share Post</span>
-              </a>
-            </div>
+            <CommentShare />
+
           </div>
           <aside className="large-4 medium-12 columns">
             <div className="ad">
